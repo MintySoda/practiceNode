b5835ec23746d30b71a299a7bf78d1480a405ba3
MintySoda <mintysodapop@gmail.com>
2018-07-29 23:05:00 -0500

    Added HTML and CSS...

--- index.js
+++ index.js
@@ -2,31 +2,14 @@ const express = require('express');
 const path = require('path');
 const e = require('events');
 
-
-const Even = new e();
 const app = express(); 
-const pathObj = path.parse(__filename);
 
 app.get('/',(req, res) =>{
     console.log("There is a GET request.");
-    res.send("Hello there!");
+    res.sendFile(__dirname + "/html/loadingPage.html");
 });
 
-app.post('/', (req,res) =>{
-    res.send("There is a POST request");
-});
-
-app.put('/', (req, res) => {
-    res.send("There is a PUT request");
-});
-
-
-console.log(pathObj);
-Even.on("addition", ()=>{
-    console.log("Let's add something.");
-});
-
-Even.emit("addition");
+app.use('/CSS/index.css', express.static(path.join(__dirname, '/CSS/index.css')));
 
 app.listen(3000,function(){
     console.log("Listening on port 3000");
